a3ee6613f9617b3a25b96148bfbf8b197f12639d
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-08-16 10:37:16 -0400

    Complete coverage on module
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- tests/test_module.py
+++ tests/test_module.py
@@ -59,3 +59,26 @@ def test_routes_construction():
     assert mod.routes
     assert 'test_doc_uri/test_protocol/1.0.0/test' in mod.routes
     assert 'test_doc_uri/test_protocol/1.0.0/test1' in mod.routes
+
+
+def test_module_type_helper():
+    """ Test that module routes are properly constructed. """
+    class TestModule(Module):
+        """ Simple module for testing """
+        DOC_URI = 'test_doc_uri/'
+        PROTOCOL = 'test_protocol'
+        VERSION = '1.0'
+
+    mod = TestModule()
+    assert mod.type('test') == 'test_doc_uri/test_protocol/1.0.0/test'
+    assert mod.type('test', doc_uri='') == 'test_protocol/1.0.0/test'
+    assert mod.type('test', protocol='protocol') == \
+        'test_doc_uri/protocol/1.0.0/test'
+    assert mod.type('test', version='2.0') == \
+        'test_doc_uri/test_protocol/2.0.0/test'
+
+
+def test_route_bad_input():
+    """ Test that calling route directly with bad inputs raises error. """
+    with pytest.raises(ValueError):
+        route([1, 2, 'garbage'])
