2b32a76714547568055418e5c0c97d8dfe5c2f1f
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-08-16 10:25:15 -0400

    Complete coverage on dispatcher
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -35,12 +35,29 @@ def test_bad_handler_invalid_type():
     """ Test malformed handler creation raises ValueError """
     with pytest.raises(ValueError):
         Handler('test', lambda: print('blah'))
+    with pytest.raises(ValueError):
+        Handler(Type.from_str('test_protocol/1.0/testing_type'), 10)
 
 
 def test_bad_handler_invalid_handler():
     """ Test malformed handler creation raises ValueError """
     with pytest.raises(ValueError):
         Handler('test', 1)
+
+
+def test_clear():
+    """ Test clearing handlers from dispatcher. """
+    dispatcher = Dispatcher()
+    dispatcher.add_handlers(
+        [
+            Handler(Type.from_str('doc;protocol/1.0/name'), lambda msg: msg),
+            Handler(Type.from_str('doc;protocol/2.0/name'), lambda msg: msg),
+        ]
+    )
+    assert dispatcher.handlers
+    dispatcher.clear_handlers()
+    assert not dispatcher.handlers
+
 
 @pytest.mark.asyncio
 async def test_dispatching():
@@ -62,6 +79,25 @@ async def test_dispatching():
     await dispatcher.dispatch(test_msg, event=called_event)
 
     assert called_event.is_set()
+
+
+@pytest.mark.asyncio
+async def test_dispatching_no_handler():
+    """ Test that routing works in agent. """
+    dispatcher = Dispatcher()
+
+    async def route_gets_called(_msg):
+        pass
+
+    dispatcher.add_handler(Handler(
+        Type.from_str('test_protocol/1.0/testing_type'), route_gets_called
+    ))
+
+    test_msg = Message({
+        '@type': 'test_protocol/4.0/other_type', 'test': 'test'
+    })
+    with pytest.raises(NoRegisteredHandlerException):
+        await dispatcher.dispatch(test_msg)
 
 
 @pytest.mark.asyncio
@@ -90,3 +126,41 @@ async def test_dispatching_selection():
     await dispatcher.dispatch(test_msg, event=called_event)
 
     assert called_event.is_set()
+
+
+@pytest.mark.asyncio
+async def test_dispatching_selection_no_appropriate_handler():
+    """ Test that routing works in agent. """
+    dispatcher = Dispatcher()
+
+    dispatcher.add_handler(Handler(
+        Type.from_str('test_protocol/2.0/testing_type'), lambda msg: msg,
+    ))
+    dispatcher.add_handler(Handler(
+        Type.from_str('test_protocol/1.0/testing_type'), lambda msg: msg,
+    ))
+
+    test_msg = Message({
+        '@type': 'test_protocol/5.0/testing_type', 'test': 'test'
+    })
+    with pytest.raises(NoRegisteredHandlerException):
+        await dispatcher.dispatch(test_msg)
+
+
+@pytest.mark.asyncio
+async def test_dispatching_selection_message_too_old():
+    """ Test that routing works in agent. """
+    dispatcher = Dispatcher()
+
+    dispatcher.add_handler(Handler(
+        Type.from_str('test_protocol/3.0/testing_type'), lambda msg: msg
+    ))
+    dispatcher.add_handler(Handler(
+        Type.from_str('test_protocol/2.0/testing_type'), lambda msg: msg
+    ))
+
+    test_msg = Message({
+        '@type': 'test_protocol/1.0/testing_type', 'test': 'test'
+    })
+    with pytest.raises(NoRegisteredHandlerException):
+        await dispatcher.dispatch(test_msg)
