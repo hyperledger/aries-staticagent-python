1b2582a25697322348af564f05f6f9577ffaebbc
Daniel Bluhm <daniel.bluhm@sovrin.org>
2020-01-22 17:42:01 -0500

    Accept base64URL values with or without padding
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -10,3 +10,14 @@ def test_create_keypair_from_seed():
     )
     assert isinstance(verkey, bytes)
     assert isinstance(sigkey, bytes)
+
+
+def test_b64_to_bytes_urlsafe_padding():
+    """Test b64 (urlsafe) decode accepts with and without padding."""
+    b64decode = crypto.b64_to_bytes
+    padded = 'dGVzdGluZyBkZWNvZGluZyB3aXRoIGFuZCB3aXRob3V0IHBhZGRpbmc='
+    unpadded = 'dGVzdGluZyBkZWNvZGluZyB3aXRoIGFuZCB3aXRob3V0IHBhZGRpbmc'
+    decoded = 'testing decoding with and without padding'
+
+    assert b64decode(padded, urlsafe=True).decode('ascii') == decoded
+    assert b64decode(unpadded, urlsafe=True).decode('ascii') == decoded
