282bab5ec0af1272aed8b3ca078f36f6c5e5a696
Daniel Bluhm <dbluhm@pm.me>
2021-09-25 15:18:21 -0400

    style: reformat examples with black
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- examples/cron.py
+++ examples/cron.py
@@ -8,20 +8,20 @@ from aries_staticagent import StaticConnection, utils
 
 from common import config
 
+
 def main():
     """Send message from cron job."""
     keys, target, _args = config()
-    conn = StaticConnection(
-        keys, target
+    conn = StaticConnection(keys, target)
+    conn.send(
+        {
+            "@type": "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/" "basicmessage/1.0/message",
+            "~l10n": {"locale": "en"},
+            "sent_time": utils.timestamp(),
+            "content": "The Cron script was executed.",
+        }
     )
-    conn.send({
-        "@type": "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/"
-                 "basicmessage/1.0/message",
-        "~l10n": {"locale": "en"},
-        "sent_time": utils.timestamp(),
-        "content": "The Cron script was executed."
-    })
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- examples/return_route_server.py
+++ examples/return_route_server.py
@@ -12,23 +12,21 @@ from aries_staticagent import StaticConnection, Target, crypto, utils
 
 def main():
     """Start a server with a static connection."""
-    their_vk, _ = crypto.create_keypair(
-        seed=hashlib.sha256(b'client').digest()
-    )
+    their_vk, _ = crypto.create_keypair(seed=hashlib.sha256(b"client").digest())
     conn = StaticConnection.from_seed(
-        hashlib.sha256(b'server').digest(),
-        Target(their_vk=their_vk)
+        hashlib.sha256(b"server").digest(), Target(their_vk=their_vk)
     )
 
-    @conn.route('https://didcomm.org/basicmessage/1.0/message')
+    @conn.route("https://didcomm.org/basicmessage/1.0/message")
     async def basic_message_auto_responder(msg, conn):
-        await conn.send_async({
-            "@type": "https://didcomm.org/"
-                     "basicmessage/1.0/message",
-            "~l10n": {"locale": "en"},
-            "sent_time": utils.timestamp(),
-            "content": "You said: {}".format(msg['content'])
-        })
+        await conn.send_async(
+            {
+                "@type": "https://didcomm.org/" "basicmessage/1.0/message",
+                "~l10n": {"locale": "en"},
+                "sent_time": utils.timestamp(),
+                "content": "You said: {}".format(msg["content"]),
+            }
+        )
 
     async def handle(request):
         """aiohttp handle POST."""
@@ -42,10 +40,10 @@ def main():
         raise web.HTTPAccepted()
 
     app = web.Application()
-    app.add_routes([web.post('/', handle)])
+    app.add_routes([web.post("/", handle)])
 
-    web.run_app(app, port=os.environ.get('PORT', 3000))
+    web.run_app(app, port=os.environ.get("PORT", 3000))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
